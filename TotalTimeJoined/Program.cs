db3572acfd134344cd299efd110a4a350e7b508c
Hanxuan Chua <Hanxuan Chua@LAPTOP-QAI2QI4H>
2021-09-21 02:17:02 +0800

    Add project files.

---
+++ TotalTimeJoined/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalTimeJoined
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of days: ");
+            int days = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of sessions per day: ");
+            int sessions = int.Parse(Console.ReadLine());
+            TotalTIme t = new TotalTIme(days, sessions);
+            Object dTime = t;
+
+            t.TotalTimeTaken(dTime);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TotalTimeJoined/TotalTIme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalTimeJoined
+{
+    class TotalTIme
+    {
+        double hours;
+        int days;
+        int sessions;
+        double sessionTime;
+
+        public TotalTIme(int days, int sessions)
+        {
+            this.days = days;
+            this.sessions = sessions;
+        }
+
+        public void TotalTimeTaken(Object t)
+        {
+            TotalTIme s = new TotalTIme(days, sessions);
+            t = s;
+
+            sessionTime = 1.5 * sessions;
+            hours = sessionTime * days;
+
+            Console.WriteLine("Total time in seconds: " + (hours * 60));
+            Console.WriteLine();
+            
+        }
+    }
+}
